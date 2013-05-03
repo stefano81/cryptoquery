dd4e502350e5259c2ab0a951a64f0b549811045b
Stefano Braghin <stefano.braghin@gmail.com>
2013-05-03 09:59:28 +0100

    restructoring the repository

--- src/schemas/lostw.c
+++ src/schemas/lostw.c
@@ -1,7 +1,30 @@
 #include "lostw.h"
 
-lostw_param * lostw_param_gen(unsigned int n) {
+lostw_setup_param * lostw_setup(unsigned int n) {
+  pbc_param_t params;
+  unsigned int rbits = 180, qbits = 603;
 
+  lostw_setup_param * params = malloc(sizeof(lostw_setup_param));
+  lostw_general_params * genparams = params->params = malloc(sizeof(lostw_general_params));
+  lostw_mpk * public = params->public = malloc(sizeof(lostw_mpk));
+  lostw_msk * secret = params->secret = mallok(sizeof(lostw_msk));
+
+  pbc_param_init_a_gen(params, 180, 603);
+  pairing_init_pbc_param(genparams->pairing, params);
+  
+  genparams->n = n;
+  element_init_G1(genparams->g, genparams->pairing);
+  element_random(genparams->g);
+
+  unsigned int N = 2 * n + 3;
+
+  element_init_GT(public->sigma, genparams->pairing);
+  pairing_apply(public->sigma, genparameters->g, genparameters->g);
+  
+  element_t dualOrthonormalBases[][] = sampleRandomOrtonormalBases(genparameters, N);
+  element_t B[]
+
+  pbc_param_clear(parmam);
 }
 
 
--- src/schemas/lostw.h
+++ src/schemas/lostw.h
@@ -3,7 +3,28 @@
 
 #include <pbc.h>
 
-lostw_param * lostw_param_gen(unsigned int n);
+typedef struct {
+  pairing_t pairing;
+  unsigned int n;
+  element_t g;
+} lostw_general_params;
+
+typedef struct {
+  element_t * B
+  element_t sigma;
+} lostw_mpk;
+
+typedef struct {
+
+} lostw_msk;
+
+typedef struct {
+  lostw_general_params * params;
+  lostw_mpk * public;
+  lostw_msk * secret;
+} lostw_setup_param;
+
+lostw_setup_param * lostw_setup(unsigned int n);
 // lostw_setup(lostw_param *);
 
 
