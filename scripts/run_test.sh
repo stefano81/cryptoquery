2678050fc1cb44f91591b5b70e60614d9ff6dea5
Stefano Braghin <stefano.braghin@gmail.com>
2013-07-15 23:36:05 +0100

    fixed the configuration for test

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-for curve in examples/param/*.param; do
-    file=data/$(date "+%Y-%m-%d-%H.%M")-$(basename $curve | cut -f1 -d.)
-    echo $curve
+if [ -z "$1" ]; then
+    for curve in examples/param/*.param; do
+	file=data/$(date "+%Y-%m-%d")-$(basename $curve | cut -f1 -d.)
+	echo $curve
+	for x in $(jot 50 2); do
+	    echo $x
+	    ./src/testing $curve $x > $file.$x.out #2> $file.$x.err
+	done
+    done
+else
     for x in $(jot 50 2); do
 	echo $x
-	./src/testing $curve $x > $file.$x.out 2> $file.$x.err
+	./src/testing $1 $x > data/$(date "+%Y-%m-%d").$(basename $1 | cut -f1 -d.).$x.out
     done
-done
+fi
