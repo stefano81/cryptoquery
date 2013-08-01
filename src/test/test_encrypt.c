5e3a03383b920eefbe729ed05edcb5c1c0795a0a
Stefano Braghin <stefano.braghin@gmail.com>
2013-08-02 00:14:07 +0100

    fix configure configuration

--- src/test/test_encrypt.c
+++ src/test/test_encrypt.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 #include <sys/time.h>
-#include <open
 
+#include <gmp.h>
 #include <pbc/pbc.h>
 #include "../schemas/utils.h"
 
@@ -18,6 +18,21 @@ int main(int argc, char **argv) {
 
   element_init_G1(m, *pairing);
   element_random(m);
+
+  element_printf("%B\n", m);
+  mpz_t n;
+
+  mpz_init(n);
+
+  element_to_mpz(n, m);
+
+  printf("%d %d %d %d %d %d\n",
+	 mpz_fits_ulong_p(n),
+	 mpz_fits_slong_p(n),
+	 mpz_fits_uint_p(n),
+	 mpz_fits_sint_p(n),
+	 mpz_fits_ushort_p(n),
+	 mpz_fits_sshort_p(n));
 
   return 0;
 }
